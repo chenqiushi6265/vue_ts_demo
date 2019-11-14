a75c0dc9ebf37d531ac802d0aa2624a7ed81ece8
chenqiushi <chenqiushi@metrodate.cn>
2019-11-14 19:48:46 +0800

    ...

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script lang="tsx">
+// import Vue, { CreateElement } from "vue";
+import { Component, Vue, Prop } from "vue-property-decorator";
+// import Component from "vue-class-component";
+
+@Component({})
+class HelloWorld extends Vue {
+  render() {
+    return (
+      <div>测试</div>
+    );
+  }
+}
+export default HelloWorld;
+
+</script>
+// <style lang="scss" scoped>
+// </style>
